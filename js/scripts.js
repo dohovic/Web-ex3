c28f695e6471b53c6c050d2f648879cdfa5cff28
Dan <dohovic@gmail.com>
2022-04-14 22:51:53 +0300

    plus functionallity rollback

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 let container;
 let animationCount = 0;
 let animationRect;
-let animationImg;
 
 function initRects() {
     for (let i = 0; i < 26; i++) {
@@ -21,58 +20,51 @@ function initRects() {
 }
 
 function animate() {
+    animationRect.setAttribute("id", "animation-rect");
     switch (animationCount) {
         case 0:
-            animationImg = document.createElement("img");
             animationRect.setAttribute("id", "animation-rect");
-            animationRect.appendChild(animationImg);
-            animationImg.src = "../images/mario/frame1.png";
             animationCount++;
             break;
         case 1:
-            animationImg.src = "../images/mario/frame2.png";
+            animationRect.style.opacity = 0.9;
             animationCount++;
             break;
         case 2:
-            animationImg.src = "../images/mario/frame3.png";
+            animationRect.style.opacity = 0.8;
             animationCount++;
             break;
         case 3:
-            animationImg.src = "../images/mario/frame4.png";
+            animationRect.style.opacity = 0.7;
             animationCount++;
             break;
         case 4:
-            animationImg.src = "../images/mario/frame5.png";
+            animationRect.style.opacity = 0.6;
             animationCount++;
             break;
         case 5:
-            animationImg.src = "../images/mario/frame6.png";
+            animationRect.style.opacity = 0.5;
             animationCount++;
             break;
         case 6:
-            animationImg.src = "../images/mario/frame5.png";
+            animationRect.style.opacity = 0.4;
             animationCount++;
             break;
         case 7:
-            animationImg.src = "../images/mario/frame7.png";
+            animationRect.style.opacity = 0.3;
             animationCount++;
             break;
         case 8: 
-            animationImg.src = "../images/mario/frame8.png";
+            animationRect.style.opacity = 0.2;
             animationCount++;
             break;
         case 9:
-            animationImg.src = "../images/mario/frame9.png";
+            animationRect.style.opacity = 0.1;
             animationCount++;
             break;
         case 10:
-            animationImg.src = "../images/mario/frame10.png";
-            animationCount++;
-            break;
-        case 11:
             animationRect.setAttribute("id", "");
             animationCount = 0;
-            animationImg.remove();
             break;
         default:
             break;
